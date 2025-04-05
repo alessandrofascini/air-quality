de7ac43a9b1700ba0fbc7239c8bc4a0aff6d61f0
Alessandro Fascini <alessandrofascini@proton.me>
2025-04-05 16:05:11 +0200

    blink internal pin

--- example/hello-esp32/src/main.rs
+++ example/hello-esp32/src/main.rs
@@ -1,10 +1,37 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::prelude::Peripherals;
 
-    // Bind the log crate to the ESP Logging facilities
+fn main() {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    // Configure Led
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = PinDriver::output(peripherals.pins.gpio2).unwrap();
+
+    loop {
+        log::info!("hello, esp32!");
+
+        // HIGH
+        match led.set_high() {
+            Ok(_) => {},
+            Err(_) => {
+                log::error!("failed to high led");
+            }
+        };
+
+        // Wait
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+
+        // LOW
+        match led.set_low() {
+            Ok(_) => {},
+            Err(_) => {
+                log::error!("failed to low led");
+            }
+        };
+
+        // Wait
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
 }
